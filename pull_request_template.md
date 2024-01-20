abb5f73d91c53fabca9b5ca131dac13b34b3f797
So-Yeon Ahn <sozygn@gmail.com>
2024-01-20 16:58:19 +0900

    initial setting

---
+++ pull_request_template.md
@@ -0,0 +1,19 @@
+## 어떤 변경인지
+
+- [ ] feat: 기능 변경, 기능 추가
+- [ ] refactor: 기능 변경 없이 코드 수정
+- [ ] style: css만 변경
+- [ ] fix: 버그 수정
+- [ ] docs: 문서 작업 (README, pr 템플릿)
+- [ ] chore: 기타 작업 (가독성 향상을 위한 줄바꿈, lint 설정 등)
+
+## 설명
+
+변경 사항을 한 문장으로 요약해주세요.
+
+### 이슈 번호
+
+> 예) .../codeit-sprint-FE2-part3-team1/probable-spoon/issues/[이슈번호]
+> https://github.com/Codeit-Rolling-11-Letsgo/probable-spoon/issues/
+
+### To Reviewers
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,59 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			md: { max: '1200px' },
+			// => @media (max-width: 767px) { ... }
+			sm: { max: '676px' },
+			// => @media (max-width: 639px) { ... }
+		},
+		fontSize: {
+			// 폰트 굵기, 높이, 줄간격, 굵기 등을 묶어서 하나의 스타일로 저장 가능
+			// <p class="text-2xl ...">The quick brown fox ...</p> 처럼 사용
+			'2xl': [
+				'1.5rem', // 폰트 크기
+				{
+					lineHeight: '2rem',
+					letterSpacing: '-0.01em',
+					fontWeight: '500',
+				},
+			],
+			'3xl': [
+				'1.875rem', // 폰트 크기
+				{
+					lineHeight: '2.25rem',
+					letterSpacing: '-0.02em',
+					fontWeight: '700',
+				},
+			],
+		},
+		extend: {
+			backgroundImage: {
+				// 자주 사용하는 그라데이션 속성,
+				// 예시) <p className="bg-gradient-radial">
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			fontFamily: {
+				// 예시) <p className="text-pretendard">
+				pretendard: ['Pretendard-Regular'],
+			},
+			colors: {
+				// 컬러픽 작성
+				// blue100: '#ECF6FF',
+				// blue101: '#F2FBFE',
+				// blue200: '#2D9CDB',
+				// blue300: '#2F80ED',
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
