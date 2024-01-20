4a3e45a1a7808012530e4790fb2082ccb38071ba
So-Yeon Ahn <sozygn@gmail.com>
2024-01-20 22:54:57 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
 	return (
 		<div>
-			<button className=' bg-sky-700 px-4  py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3'>...</button>
+			<button className=' bg-sky-700 px-4  py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3'>
+				... Main 브랜치로 직접 Push 하는 것이 막혔는지 확인
+			</button>
 		</div>
 	);
 }
