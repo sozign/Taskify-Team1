7bad5f5ca3516d03e65ae4da5c7b4991fb6123dc
So-Yeon Ahn <sozygn@gmail.com>
2024-01-20 23:05:46 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
 	return (
 		<div>
-			<button className=' bg-sky-700 px-4  py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3'>
-				... Main 브랜치로 직접 Push 하는 것이 막혔는지 확인 2차 확인 3차 확인
-			</button>
+			<button className=' bg-sky-700 px-4  py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3'>...</button>
 		</div>
 	);
 }
