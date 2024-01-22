47983f2cd8a56390dd14a790adcef9e105ac8451
박지원 <jackgg12322@gmail.com>
2024-01-22 22:27:11 +0900

    style : tailwindConfigColorCustom

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,14 +38,51 @@ const config: Config = {
 				// 자주 사용하는 그라데이션 속성,
 				// 예시) <p className="bg-gradient-radial">
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
 			fontFamily: {
 				// 예시) <p className="text-pretendard">
 				pretendard: ['Pretendard-Regular'],
 			},
 			colors: {
+				black: {
+					DEFAULT: '#000000',
+					1: '#171717',
+					2: '#333236',
+					3: '#4B4B4B',
+				},
+				gray: {
+					DEFAULT: '#787486',
+					1: '#9FA6B2',
+					2: '#D9D9D9',
+					3: '#EEEEEE',
+					4: '#FAFAFA',
+				},
+				white: {
+					DEFAULT: '#FFFFFF',
+				},
+				violet: {
+					DEFAULT: '#5534DA',
+					1: '#F1EFFD',
+				},
+				red: {
+					DEFAULT: '#D6173A',
+				},
+				green: {
+					DEFAULT: '#7AC555',
+				},
+				purple: {
+					DEFAULT: '#760DDE',
+				},
+				orange: {
+					DEFAULT: '#FFA500',
+				},
+				blue: {
+					DEFAULT: '#76A5EA',
+				},
+				pink: {
+					DEFAULT: '#E876EA',
+				},
 				// 컬러픽 작성
 				// blue100: '#ECF6FF',
 				// blue101: '#F2FBFE',
