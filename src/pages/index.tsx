883835d341fd77b89cebacc25dd31defe1c39909
So-Yeon Ahn <sozygn@gmail.com>
2024-01-22 13:40:54 +0900

    text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ export default function Home() {
 	return (
 		<div>
 			<button className=' bg-sky-700 px-4  py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3'>...</button>
+			배포 자동화 확인 하기
 		</div>
 	);
 }
