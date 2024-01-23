55926dc7b08c85b85398573799f81c3395e03a8e
So-Yeon Ahn <sozygn@gmail.com>
2024-01-23 14:58:31 +0900

    style: breakpoint 지정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div>
-			<button className=' bg-sky-700 px-4  py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3'>...</button>
+			<button className=' bg-sky-800 px-4  py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3'>...</button>
 		</div>
 	);
 }
