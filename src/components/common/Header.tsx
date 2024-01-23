142f15e884de2ede2b2791de7a076a26bbfa2167
박지원 <jackgg12322@gmail.com>
2024-01-23 16:51:58 +0900

    feat : Public 폴더 생성및headerComponent 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,24 +1,26 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import TaskifyImage from '../../../TaskifyImage.svg';
-import Taskify from '../../../Taskify.svg';
+import TaskifyImage from '../../../Public/assets/TaskifyImage.svg';
+import Taskify from '../../../Public/assets/Taskify.svg';
 
 export default function Header() {
 	return (
 		<header className='bg-white fixed inset-x-0 top-0 z-50 left-0'>
 			<div className='container mx-auto flex flex-wrap p-5  justify-between md:flex-row items-center'>
-				<div className='flex font-medium items-center mb-4 md:mb-0'>
-					<Image alt='TaskifyImage' src={TaskifyImage} className='w-8 h-8 -mr-1' />
-					<span className='ml-3 text-2xl'>
-						{/* { ? <Image alt='이미지 없음' src={null}/> :<Image alt='TaskifyLogo' src={Taskify} />} */}
-					</span>
-				</div>
+				<Link href='/' className='cursor-pointer'>
+					<div className='flex font-medium items-center mb-4 md:mb-0'>
+						<Image alt='TaskifyImage' src={TaskifyImage} className='w-8 h-8 -mr-1' />
+						<span className='ml-3 text-2xl'>
+							<Image alt='TaskifyLogo' src={Taskify} className='sm:hidden' />
+						</span>
+					</div>
+				</Link>
 				<nav className='md:ml-auto flex flex-wrap items-center justify-center'>
-					<div className='mr-5 hover:text-gray-900'>
-						<Link href=''>로그인</Link>
+					<div className='mr-5 hover:'>
+						<Link href='/login'>로그인</Link>
 					</div>
 					<div className='mr-5 hover:text-gray-900'>
-						<Link href=''>회원가입</Link>
+						<Link href='/signup'>회원가입</Link>
 					</div>
 				</nav>
 			</div>
