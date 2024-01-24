e7e1e51a9615a47c4d51859d305a5de7f15a076f
unknown <llqrw55@naver.com>
2024-01-24 20:05:52 +0900

    style/JIS0098/17

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
 import Header from '@/components/common/Header';
+import Button from '@/components/common/Button';
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<Button color='violet' variant='confirm'>
+				수락
+			</Button>
+			<Button color='white' variant='confirm'>
+				거절
+			</Button>
+			<Button color='white' variant='delete-lg'>
+				삭제
+			</Button>
+			<Button color='white' variant='delete-sm'>
+				삭제
+			</Button>
+			<Button color='white' variant='commentInput'>
+				입력
+			</Button>
+			<Button color='modalWhite' variant='modal'>
+				취소
+			</Button>
+			<Button color='modalViolet' variant='modal'>
+				확인
+			</Button>
 		</div>
 	);
 }
