4f5f081021dba87ae9aeea85cd52457ad66dd491
hyun522 <jhj1004v@naver.com>
2024-01-24 21:28:36 +0900

    CHORE : react-select 설치

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import DropDownManager from '@/components/DropDownManager ';
 import Header from '@/components/common/Header';
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<DropDownManager />
 		</div>
 	);
 }
