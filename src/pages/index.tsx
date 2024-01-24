22ec32247a0b67ab190c80e92089e6807c0bf5c1
hyun522 <jhj1004v@naver.com>
2024-01-24 22:59:16 +0900

    FEAT : manager 드롭다운 / search  구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import DropDownManager from '@/components/DropDownManager ';
+import DropDownManager from '@/components/DropDownManager';
 import Header from '@/components/common/Header';
 export default function Home() {
 	return (
