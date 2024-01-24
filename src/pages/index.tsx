83d42449321308f58e2c9be19a9c57d28c21009d
unknown <llqrw55@naver.com>
2024-01-24 19:59:06 +0900

    style/JIS0098/17

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
+import Button from '@/components/common/Buttons/Button';
+
 export default function Home() {
-	return <div>랜딩페이지</div>;
+	return (
+		<div>
+			<Button color='violet' variant='confirm'>
+				수락
+			</Button>
+			<Button color='white' variant='confirm'>
+				거절
+			</Button>
+			<Button color='white' variant='delete-lg'>
+				삭제
+			</Button>
+			<Button color='white' variant='delete-sm'>
+				삭제
+			</Button>
+			<Button color='white' variant='commentInput'>
+				입력
+			</Button>
+			<Button color='modalWhite' variant='modal'>
+				취소
+			</Button>
+			<Button color='modalViolet' variant='modal'>
+				확인
+			</Button>
+		</div>
+	);
 }
