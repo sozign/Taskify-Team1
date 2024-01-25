39e892f3bbaa186c48e332e4fb3bc09fe1422e0a
So-Yeon Ahn <sozygn@gmail.com>
2024-01-25 19:40:00 +0900

    style: 모달 레이아웃 title props추가, border radius 조정

--- src/components/modal/Layout.tsx
+++ src/components/modal/Layout.tsx
@@ -4,17 +4,18 @@ import { createPortal } from 'react-dom';
 interface LayoutProps {
 	children: ReactNode;
 	$modalType: 'Alert' | 'Modal' | 'Task';
+	title?: string;
 	isOpen: boolean;
 	setOpen: Dispatch<SetStateAction<boolean>>;
 }
-function Layout({ children, $modalType, isOpen, setOpen }: LayoutProps) {
+function Layout({ children, $modalType, title, isOpen, setOpen }: LayoutProps) {
 	if (!isOpen) return null;
 	const modalType = {
 		Alert:
-			'flex h-[25rem] w-[54rem] justify-center rounded-lg bg-white px-[2.8rem] pt-[10.8rem] sm:h-[22rem] sm:w-[32.7rem] sm:pt-[8.1rem]',
+			'rounded-[0.8rem] flex h-[25rem] w-[54rem] justify-center rounded-lg bg-white px-[2.8rem] pt-[10.8rem] sm:h-[22rem] sm:w-[32.7rem] sm:pt-[8.1rem]',
 		Modal:
-			'flex min-h-[22rem] h-fit w-[54rem] flex-col bg-white px-[2.8rem] pb-[2.8rem] pt-[3.2rem] sm:px-[2rem] sm:py-[2.8rem]',
-		Task: 'flex h-fit w-[73rem] flex-shrink-0 flex-col bg-white px-[2.8rem] py-[3.2rem] md:h-[77rem] md:w-[68rem] sm:h-[70.8rem] sm:w-[32.7rem] sm:px-[4rem] sm:pb-[2.8rem] sm:pt-[4rem]',
+			'rounded-[0.8rem] flex min-h-[22rem] h-fit w-[54rem] flex-col bg-white px-[2.8rem] pb-[2.8rem] pt-[3.2rem] sm:px-[2rem] sm:py-[2.8rem]',
+		Task: 'rounded-[0.8rem] flex h-fit w-[73rem] flex-shrink-0 flex-col bg-white px-[2.8rem] py-[3.2rem] md:h-[77rem] md:w-[68rem] sm:h-[70.8rem] sm:w-[32.7rem] sm:px-[4rem] sm:pb-[2.8rem] sm:pt-[4rem]',
 	};
 
 	return createPortal(
@@ -28,6 +29,7 @@ function Layout({ children, $modalType, isOpen, setOpen }: LayoutProps) {
 					e.stopPropagation();
 				}}
 			>
+				{$modalType === 'Modal' && <p className='mb-[3.2rem] text-24-700 text-black-3'>{title}</p>}
 				{children}
 			</div>
 		</div>,
