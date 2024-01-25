8e996cdeb032b394d13558117ad638bcbdbaeded
박지원 <jackgg12322@gmail.com>
2024-01-26 02:23:39 +0900

    style : 마이대시보드헤더 스타일 수정

--- src/components/common/Headers/MyDashboardHeader.tsx
+++ src/components/common/Headers/MyDashboardHeader.tsx
@@ -14,7 +14,7 @@ export default function MyDashboardHeader({ title, nickname, profileImageUrl }: 
 		<header className='container m-3 border-b-[0.1rem] bg-white pb-3 pl-[20.2%] '>
 			<div className='flex items-center justify-between'>
 				<Link href='/mydashboard'>
-					<div className='flex items-center justify-center gap-2 pl-[4.2rem] text-18-700 md:hidden sm:hidden'>
+					<div className='flex items-center justify-center gap-2 text-18-700 md:hidden sm:hidden'>
 						{title}
 						<Image alt='대시보드 왕관 아이콘' src={RoyalCrownIcon} />
 					</div>
@@ -22,22 +22,19 @@ export default function MyDashboardHeader({ title, nickname, profileImageUrl }: 
 				<nav>
 					<div className='m-[1rem] flex gap-5 pr-[1em]'>
 						<Image alt='영역 나누는 라인 이미지' src={Vector} />
-
-						<div className='flex items-center gap-1'>
-							<Link href='/mypage'>
-								<div className='inline'>
-									<Image
-										alt='프로필 이미지'
-										src={profileImageUrl}
-										className='relative inline h-[2.375rem] w-[2.375rem] rounded-[50rem] bg-gray-7'
-									/>
-									<span className='absolute  z-10 translate-x-[-50%] text-12-600 text-white'>
-										{nickname.slice(0, 1)}
-									</span>
-								</div>
-								<span className='text-12-500 sm:hidden'>{nickname}</span>
-							</Link>
-						</div>
+						<Link href='/mypage' className='flex items-center justify-center gap-2'>
+							<div className=''>
+								<Image
+									alt='프로필 이미지'
+									src={profileImageUrl}
+									className='relative h-[3.8rem] w-[3.8rem] rounded-[3rem] border-2 border-solid border-white bg-black-0 '
+								/>
+								<span className='absolute z-10 translate-x-[150%] translate-y-[-130%] text-16-600  text-white'>
+									{nickname.slice(0, 1)}
+								</span>
+							</div>
+							<span className='text-16-600 sm:hidden'>{nickname}</span>
+						</Link>
 					</div>
 				</nav>
 			</div>
--- src/pages/dashboard/[boardid]/edit.tsx
+++ src/pages/dashboard/[boardid]/edit.tsx
@@ -1,9 +1,9 @@
-import DashboardHeader from '@/components/common/Headers/DashboardHeader';
+import MyDashboardHeader from '@/components/common/Headers/MyDashboardHeader';
 
 export default function DashBoardEdit() {
 	return (
 		<div>
-			<DashboardHeader id={0} nickname={'nickname'} profileImageUrl={''} title={'비브리지'} />내 대시보드 수정 페이지
+			<MyDashboardHeader title={'비브리지'} nickname={'nickname'} profileImageUrl={''} />내 대시보드 수정 페이지
 		</div>
 	);
 }
