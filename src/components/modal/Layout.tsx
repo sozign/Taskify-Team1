62648cda7a201bccf58084251462fe52fb32a531
박운성 <cheanju0@gmail.com>
2024-01-25 17:01:22 +0900

    refactor: modalLayout 수정

--- src/components/modal/Layout.tsx
+++ src/components/modal/Layout.tsx
@@ -1,31 +1,22 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-function Layout({
-	children,
-	$modalType,
-	primaryButton = null,
-	secondaryButton = null,
-}: {
+interface LayoutProps {
 	children: ReactNode;
 	$modalType: 'Alert' | 'Modal' | 'Task';
-	primaryButton?: '확인' | '생성' | '변경' | '삭제' | '초대' | null;
-	secondaryButton?: '취소' | null;
-}) {
+}
+
+function Layout({ children, $modalType }: LayoutProps) {
 	const modalType = {
-		Alert: 'flex h-[25rem] w-[54rem] items-center justify-center rounded-lg bg-white px-[2.8rem] py-[2.8rem]',
-		Modal: 'flex min-h-[22rem] min-w-[32.7rem] h-fit w-fit flex-col bg-white px-[2.8rem] pb-[2.8rem] pt-[3.2rem]',
-		Task: 'flex h-[76.3rem] w-[73rem] flex-shrink-0 flex-col bg-white px-[2.8rem] py-[3.2rem]',
+		Alert:
+			'flex h-[25rem] w-[54rem] items-center justify-center rounded-lg bg-white px-[2.8rem] py-[2.8rem] sm:h-[22rem] sm:w-[32.7rem]',
+		Modal:
+			'flex min-h-[22rem] h-fit w-[54rem] flex-col bg-white px-[2.8rem] pb-[2.8rem] pt-[3.2rem] sm:px-[2rem] sm:py-[2.8rem]',
+		Task: 'flex h-fit w-[73rem] flex-shrink-0 flex-col bg-white px-[2.8rem] py-[3.2rem] md:h-[77rem] md:w-[68rem] sm:h-[70.8rem] sm:w-[32.7rem] sm:px-[4rem] sm:pb-[2.8rem] sm:pt-[4rem]',
 	};
 
 	return (
 		<div className='overlay fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black-0/70'>
-			<div className={`modal relative z-10 ${modalType[$modalType]}`}>
-				{children}
-				<div className={`buttonBox absolute bottom-[2.8rem] right-[2.8rem] flex flex-row-reverse gap-[1.2rem]`}>
-					{primaryButton ? <button>{primaryButton}</button> : primaryButton}
-					{secondaryButton ? <button>{secondaryButton}</button> : secondaryButton}
-				</div>
-			</div>
+			<div className={`modal relative z-10 ${modalType[$modalType]}`}>{children}</div>
 		</div>
 	);
 }
