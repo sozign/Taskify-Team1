c26df9472abd73040e8bc9d26c06fb8a05b70315
hyun522 <jhj1004v@naver.com>
2024-01-27 02:45:13 +0900

    index.tsx에서 컴포넌트 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import DropDownManager from '@/components/DropDownManager';
 import Header from '@/components/common/Header';
 export default function Home() {
 	return (
 		<div>
 			<Header />
-			<DropDownManager />
 		</div>
 	);
 }
