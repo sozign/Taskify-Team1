02ac03cb170ad93c91d9b5ec0363f8d1791fe0ad
박운성 <cheanju0@gmail.com>
2024-01-26 19:22:39 +0900

    feat: api 추가

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,13 +1,15 @@
 export interface UserInfo {
+	id: number;
+	email: string;
+	nickname: string;
+	profileImageUrl: null | string;
+	createdAt: string;
+	updatedAt: string;
+}
+
+export interface UserInfoWithToken {
 	accessToken: string;
-	user: {
-		id: number;
-		email: string;
-		nickname: string;
-		profileImageUrl: null | string;
-		createdAt: string;
-		updatedAt: string;
-	};
+	user: UserInfo;
 }
 
 export interface CardItemPost {
@@ -21,7 +23,7 @@ export interface CardItemPost {
 	imageUrl: string;
 }
 
-export interface CardItemGet {
+export interface CardData {
 	id: number;
 	title: string;
 	description: string;
@@ -39,8 +41,148 @@ export interface CardItemGet {
 	updatedAt: string;
 }
 
-export interface CardList {
+export interface CardItemGet {
 	cursorId: number;
 	totalCount: number;
-	cards: CardItemGet[];
+	cards: CardData[];
+}
+
+export interface ColumnPost {
+	title: string;
+	dashboardId: number;
+}
+
+export interface ColumnData {
+	id: number;
+	title: string;
+	teamId: string;
+	createdAt: string;
+	updatedAt: string;
+}
+
+export interface ColumnGet {
+	result: string;
+	data: ColumnData[];
+}
+
+export interface ColumnPut {
+	title: string;
+}
+
+export interface ImagePost {
+	image: string;
+}
+
+export interface CardImageGet {
+	imageUrl: string;
+}
+
+export interface CommnetPost {
+	content: string;
+	cardId: number;
+	columnId: number;
+	dashboardId: number;
+}
+
+export interface CommentData {
+	id: number;
+	content: string;
+	createdAt: string;
+	updatedAt: string;
+	cardId: number;
+	author: {
+		profileImageUrl: string;
+		nickname: string;
+		id: number;
+	};
+}
+
+export interface CommentsGet {
+	cursorId: number;
+	comments: CommentData[];
+}
+
+export interface CommentPut {
+	content: string;
+}
+
+export interface DashboardPost {
+	title: string;
+	color: string;
+}
+
+export interface DashboardData {
+	id: number;
+	title: string;
+	color: string;
+	createdAt: string;
+	updatedAt: string;
+	createdByMe: boolean;
+	userId: number;
+}
+
+export interface DashboardsGet {
+	cursorId: number;
+	totalcount: string;
+	dashboards: DashboardData[];
+}
+
+export interface InvitationDashboardPost {
+	email: string;
+}
+
+export interface InvitationDashboardData {
+	id: number;
+	inviter: {
+		nickname: string;
+		email: string;
+		id: number;
+	};
+	teamId: string;
+	dashboard: {
+		title: string;
+		id: number;
+	};
+	invitee: {
+		nickname: string;
+		email: string;
+		id: number;
+	};
+	inviteAccepted: boolean;
+	createdAt: string;
+	updatedAt: string;
+}
+
+export interface InvitationsDashboardGet {
+	totalCount: number;
+	invitations: InvitationDashboardData[];
+}
+
+export interface InvitationsGet {
+	cursorId: number;
+	invitations: InvitationDashboardData[];
+}
+
+export interface InvitationPut {
+	inviteAccepted: boolean;
+}
+
+export interface MembersData {
+	id: number;
+	userId: number;
+	email: string;
+	nickname: string;
+	profileImageUrl: string;
+	createdAt: string;
+	updatedAt: string;
+	isOwner: boolean;
+}
+
+export interface MembersGet {
+	members: MembersData[];
+	totalCount: number;
+}
+
+export interface UsersProfileImagePost {
+	profileImageUrl: string;
 }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,30 @@
-import { CardItemGet, CardItemPost, CardList, UserInfo } from '@/constants/types';
+import {
+	CardImageGet,
+	ImagePost,
+	CardData,
+	CardItemPost,
+	CardItemGet,
+	ColumnData,
+	ColumnGet,
+	ColumnPost,
+	ColumnPut,
+	CommentData,
+	CommentsGet,
+	CommentPut,
+	CommnetPost,
+	DashboardData,
+	DashboardsGet,
+	DashboardPost,
+	InvitationsDashboardGet,
+	InvitationDashboardData,
+	InvitationDashboardPost,
+	UserInfoWithToken,
+	InvitationsGet,
+	InvitationPut,
+	MembersGet,
+	UserInfo,
+	UsersProfileImagePost,
+} from '@/constants/types';
 import { authInstance as authAxios, instance as axios } from './axios';
 
 //===============================
@@ -12,8 +38,11 @@ interface postAuthLoginProps {
 	password: string;
 }
 export async function postAuthLogin({ email, password }: postAuthLoginProps) {
-	const res = await axios.post('/auth/login', { email, password });
-	return res.data as UserInfo;
+	const res = await axios.post<UserInfoWithToken>('/auth/login', {
+		email,
+		password,
+	});
+	return res.data;
 }
 
 /**
@@ -24,8 +53,11 @@ interface putAuthPasswordProps {
 	newPassword: string;
 }
 export async function putAuthPassword({ password, newPassword }: putAuthPasswordProps) {
-	const res = await authAxios.put('/auth/password', { password, newPassword });
-	return res.status as number; //204 성공
+	const res = await authAxios.put<number>('/auth/password', {
+		password,
+		newPassword,
+	});
+	return res.status; //204 성공
 }
 
 //===============================
@@ -35,39 +67,303 @@ export async function putAuthPassword({ password, newPassword }: putAuthPassword
  * 카드 생성
  */
 export async function postCards(cardItem: CardItemPost) {
-	const res = await authAxios.post('/cards', cardItem);
-	return res.data as CardItemGet;
+	const res = await authAxios.post<CardData>('/cards', cardItem);
+	return res.data;
+}
+
+interface getCardsProps {
+	size?: number;
+	cursorId?: number;
+	columnId: number;
 }
 /**
  * 카드 목록 조회
  */
-interface getCardsProps {
-	size: number;
-	cursorId: number;
-	columnId: number;
+export async function getCards({ size = 10, cursorId, columnId }: getCardsProps) {
+	const res = await authAxios.get<CardItemGet>(`/cards?size=${size}&cursorId=${cursorId}&columnId=${columnId}`);
+	return res.data;
 }
-export async function getCards({ size, cursorId, columnId }: getCardsProps) {
-	const res = await authAxios.get(`/cards?size=${size}&cursorId=${cursorId}&columnId=${columnId}`);
-	return res.data as CardList;
-}
+
 /**
  * 카드 수정
  */
 export async function putCardItem(cardId: number, cardItem: CardItemPost) {
-	const res = await authAxios.put(`/cards/${cardId}`, cardItem);
-	return res.data as CardItemGet;
+	const res = await authAxios.put<CardData>(`/cards/${cardId}`, cardItem);
+	return res.data;
 }
+
 /**
  * 카드 상세조회
  */
 export async function getCardItem(cardId: number) {
-	const res = await authAxios.get(`/cards/${cardId}`);
-	return res.data as CardItemGet;
+	const res = await authAxios.get<CardData>(`/cards/${cardId}`);
+	return res.data;
 }
+
 /**
  * 카드 삭제
  */
 export async function deleteCardItem(cardId: number) {
-	const res = await authAxios.delete(`/cards/${cardId}`);
-	return res.status as number;
+	const res = await authAxios.delete<number>(`/cards/${cardId}`);
+	return res.status;
+}
+
+//===============================
+//===========Columns===============
+//===============================
+/**
+ * 컬럼 생성
+ */
+export async function postColumn(Column: ColumnPost) {
+	const res = await authAxios.post<ColumnData>('/columns', Column);
+	return res.data;
+}
+
+/**
+ * 컬럼 목록 조회
+ */
+export async function getColumns(dashboardId: number) {
+	const res = await authAxios.get<ColumnGet>(`/columns?dashboardId=${dashboardId}`);
+	return res.data;
+}
+
+/**
+ * 컬럼 수정
+ */
+export async function putColumn(columnId: number, Column: ColumnPut) {
+	const res = await authAxios.put<ColumnData>(`/columns/${columnId}`, Column);
+	return res.data;
+}
+
+/**
+ * 컬럼 삭제
+ */
+export async function deleteColumn(columnId: number) {
+	const res = await authAxios.delete<number>(`/columns/${columnId}`);
+	return res.status;
+}
+
+/**
+ * 카드 이미지 업로드
+ */
+export async function postCardImage(columnId: number, imageFile: ImagePost) {
+	const res = await authAxios.post<CardImageGet>(`/columns/${columnId}/card-image`, imageFile);
+	return res.data;
+}
+
+//===============================
+//===========Comments===============
+//===============================
+/**
+ * 댓글 생성
+ */
+export async function postComment(comment: CommnetPost) {
+	const res = await authAxios.post<CommentData>('/comments', comment);
+	return res.data;
+}
+
+interface getCommentsProps {
+	size?: number;
+	cursorId?: number;
+	cardId: number;
+}
+
+/**
+ * 댓글 목록 조회
+ */
+export async function getComments({ size = 10, cursorId, cardId }: getCommentsProps) {
+	const res = await authAxios.get<CommentsGet>(`/comments?size=${size}&cursorId=${cursorId}&cardId=${cardId}`);
+	return res.data;
+}
+
+/**
+ * 댓글 수정
+ */
+export async function putComment(commentId: number, comment: CommentPut) {
+	const res = await authAxios.put<CommentData>(`/comments/${commentId}`, comment);
+	return res.data;
+}
+
+/**
+ * 댓글 삭제
+ */
+export async function deleteComment(commentId: number) {
+	const res = await authAxios.delete<number>(`/comments/${commentId}`);
+	return res.status;
+}
+
+//===============================
+//===========Dashboards===============
+//===============================
+/**
+ * 대시보드 생성
+ */
+export async function postDashboard(dashboard: DashboardPost) {
+	const res = await authAxios.post<DashboardData>(`/dashboards`, dashboard);
+	return res.data;
+}
+
+interface getDashboardsProps {
+	navigationMethod: 'infiniteScroll' | 'pagination';
+	cursorId?: number;
+	page?: number;
+	size?: number;
+}
+
+/**
+ * 대시보드 목록 조회
+ */
+export async function getDashboards({ navigationMethod, cursorId, page = 1, size = 10 }: getDashboardsProps) {
+	const res = await authAxios.get<DashboardsGet>(
+		`/dashboards?navigationMethod=${navigationMethod}&cursorId=${cursorId}&page=${page}&size=${size}`,
+	);
+	return res.data;
+}
+
+/**
+ * 대시보드 상세 조회
+ */
+export async function getDashboardItem(dashboardId: number) {
+	const res = await authAxios.get<DashboardData>(`/dashboards/${dashboardId}`);
+	return res.data;
+}
+
+/**
+ * 대시보드 수정
+ */
+export async function putDashboard(dashboardId: number, dashboard: DashboardPost) {
+	const res = await authAxios.put<DashboardData>(`/dashboards/${dashboardId}`, dashboard);
+	return res.data;
+}
+
+/**
+ * 대시보드 삭제
+ */
+export async function deleteDashboard(dashboardId: number) {
+	const res = await authAxios.delete<number>(`/dashboards/${dashboardId}`);
+	return res.status;
+}
+
+/**
+ * 대시보드 초대하기
+ */
+export async function postInvitationDashboard(dashboardId: number, invitation: InvitationDashboardPost) {
+	const res = await authAxios.post<InvitationDashboardData>(`/dashboards/${dashboardId}/invitations`, invitation);
+	return res.data;
+}
+
+interface getInvitationsDashboardProps {
+	dashboardId: number;
+	page: number;
+	size: number;
+}
+
+/**
+ * 대시보드 초대 불러오기
+ */
+export async function getInvitationsDashboard({ dashboardId, page, size }: getInvitationsDashboardProps) {
+	const res = await authAxios.get<InvitationsDashboardGet>(
+		`/dashboards/${dashboardId}/invitations?page=${page}&size=${size}`,
+	);
+	return res.data;
+}
+
+/**
+ * 대시보드 초대 취소
+ */
+export async function deleteInvitationDashboard(dashboardId: number, invitationId: number) {
+	const res = await authAxios.delete<number>(`/dashboards/${dashboardId}/invitations/${invitationId}`);
+	return res.status;
+}
+
+//===============================
+//===========Invitations===============
+//===============================
+interface getInvitationsProps {
+	size?: number;
+	cursorId?: number;
+	title?: string;
+}
+/**
+ * 내가 받은 초대 목록 조회
+ */
+export async function getInvitations({ size, cursorId, title }: getInvitationsProps) {
+	const res = await authAxios.get<InvitationsGet>(`/invitations?size=${size}&cursorId=${cursorId}&title=${title}`);
+	return res.data;
+}
+
+/**
+ * 초대 응답
+ */
+export async function putInvitations(invitationId: number, inviteAccepted: InvitationPut) {
+	const res = await authAxios.put<InvitationsGet>(`/invitations/${invitationId}`, inviteAccepted);
+	return res.data;
+}
+
+//===============================
+//===========Members===============
+//===============================
+interface getMembersProps {
+	page?: number;
+	size?: number;
+	dashboardId: number;
+}
+/**
+ * 대시보드 멤버 목록 조회
+ */
+export async function getMembers({ page, size, dashboardId }: getMembersProps) {
+	const res = await authAxios.get<MembersGet>(`/members?page=${page}&size=${size}&dashboardId=${dashboardId}`);
+	return res.data;
+}
+
+/**
+ * 대시보드 멤버 삭제
+ */
+export async function deleteMembers(memberId: number) {
+	const res = await authAxios.delete<number>(`/members/${memberId}`);
+	return res.status;
+}
+
+//===============================
+//===========Users===============
+//===============================
+interface postUsersProps {
+	email: string;
+	nickname: string;
+	password: string;
+}
+/**
+ * 회원가입
+ */
+export async function postUsers(userInfo: postUsersProps) {
+	const res = await authAxios.post<UserInfo>(`/users`, userInfo);
+	return res.data;
+}
+
+/**
+ * 내 정보 조회
+ */
+export async function getUsers() {
+	const res = await authAxios.get<UserInfo>(`/users/me`);
+	return res.data;
+}
+
+interface putUsersProps {
+	nickname: string;
+	profileImageUrl: string;
+}
+/**
+ * 내 정보 수정
+ */
+export async function putUsers(userData: putUsersProps) {
+	const res = await authAxios.put<UserInfo>(`/users/me`, userData);
+	return res.data;
+}
+
+/**
+ * 프로필 이미지 업로드
+ */
+export async function postUsersProfileImage(imageFile: ImagePost) {
+	const res = await authAxios.post<UsersProfileImagePost>(`/users/me/image`, imageFile);
+	return res.data;
 }
