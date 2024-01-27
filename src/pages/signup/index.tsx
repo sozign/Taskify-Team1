6f8e0ea99346112ffda13b32aac310651601a776
박지원 <jackgg12322@gmail.com>
2024-01-27 20:08:59 +0900

    feat : 로그아웃 페이지 구현중

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,3 +1,118 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { useForm } from 'react-hook-form';
+import AuthInput from '@/components/common/Input/AuthInput';
+import { VALIDATE_RULES } from '@/constants/validation';
+import AuthButton from '@/components/common/Buttons/AuthButton';
+import Taskify from '@/../../Public/assets/Taskify.svg';
+import TaskifyImg from '@/../../Public/assets/TaskifyImage.svg';
+import { useState } from 'react';
+
+type SignupFormData = {
+	email: string;
+	nickname: string;
+	password: string;
+	validPassword: string;
+};
+
 export default function Signup() {
-	return <div>회원가입 페이지</div>;
+	const [isCheckboxChecked, setCheckboxChecked] = useState<boolean>(false);
+
+	const handleCheckboxChange = () => {
+		setCheckboxChecked(!isCheckboxChecked);
+	};
+	const {
+		register,
+		handleSubmit,
+		// setError,
+		formState: { errors },
+	} = useForm<SignupFormData>({
+		mode: 'onBlur',
+		defaultValues: {
+			email: '',
+			nickname: '',
+			password: '',
+			validPassword: '',
+		},
+	});
+
+	const INPUT_SETTING = {
+		label: {
+			email: '이메일',
+			nickname: '닉네임',
+			password: '비밀번호',
+			validPassword: '비밀번호 확인',
+		},
+		placeholder: {
+			email: '이메일을 입력해 주세요.',
+			nickname: '닉네임을 입력해 주세요.',
+			password: '비밀번호를 입력해 주세요.',
+			validPassword: '비밀번호를 한번 더 입력해 주세요.',
+		},
+	};
+
+	const onSubmit = (data: SignupFormData) => {
+		console.log(data);
+	};
+
+	return (
+		<div className='grid translate-y-[17rem] place-items-center'>
+			<Link href='/'>
+				<div className='flex h-[22rem] w-[12.5rem] shrink-0 flex-col items-center justify-center gap-2  pl-[0.01rem]'>
+					<Image alt='Taskify 로고 이미지' src={TaskifyImg} className='h-[15rem] w-[13rem] shrink-0' />
+					<Image alt='Taskify 로고' src={Taskify} className='h-[3.4rem] w-[13rem] shrink-0' />
+				</div>
+			</Link>
+			<p className='text-center text-12-500 text-black-3'>오늘도 만나서 반가워요 !</p>
+			<form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-center gap-[6rem]'>
+				<AuthInput
+					type='email'
+					required={!!VALIDATE_RULES.email?.required}
+					label={INPUT_SETTING.label.email}
+					placeholder={INPUT_SETTING.placeholder.email}
+					errorMessage={errors?.email?.message}
+					{...register('email', VALIDATE_RULES.email)}
+					className='h-[4.81rem] w-[52rem] gap-2  sm:px-[8.2rem]'
+				/>
+				<AuthInput
+					type='text'
+					required={!!VALIDATE_RULES.nickname?.required}
+					label={INPUT_SETTING.label.nickname}
+					placeholder={INPUT_SETTING.placeholder.nickname}
+					errorMessage={errors?.nickname?.message}
+					{...register('nickname', VALIDATE_RULES.nickname)}
+					className='h-[4.81rem] w-[52rem] gap-2  sm:px-[8.2rem]'
+				/>
+				<AuthInput
+					type='password'
+					required={!!VALIDATE_RULES.passwordInSignup?.required}
+					label={INPUT_SETTING.label.password}
+					placeholder={INPUT_SETTING.placeholder.password}
+					errorMessage={errors?.password?.message}
+					{...register('password', VALIDATE_RULES.passwordInSignup)}
+					className='h-[4.81rem] w-[52rem] gap-2 sm:px-[8.2rem]'
+				/>
+				<AuthInput
+					type='password'
+					required={!!VALIDATE_RULES.passwordInSignup?.required}
+					label={INPUT_SETTING.label.validPassword}
+					placeholder={INPUT_SETTING.placeholder.validPassword}
+					errorMessage={errors?.validPassword?.message}
+					{...register('validPassword', VALIDATE_RULES.passwordInSignup)}
+					className='h-[4.81rem] w-[52rem] gap-2 sm:px-[8.2rem]'
+				/>
+				<div className='flex items-center gap-1'>
+					<input type='checkbox' checked={isCheckboxChecked} onChange={handleCheckboxChange} />
+					<label>이용약관에 동의합니다.</label>
+				</div>
+				<AuthButton disabled={!isCheckboxChecked}>가입하기</AuthButton>
+			</form>
+			<p className='py-5 text-12-400 text-black-3'>
+				이미 가입하셨나요?
+				<Link href='/login' className='underline'>
+					로그인 하기
+				</Link>
+			</p>
+		</div>
+	);
 }
