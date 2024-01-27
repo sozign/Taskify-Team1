857d749ef1d4ace0981aff428c18f068735f82d0
hyun522 <jhj1004v@naver.com>
2024-01-27 20:11:51 +0900

    FIX : conflit 해결 - index.tsx  컴포넌트 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import LandingHeader from '@/components/common/Headers/LandingHeader';
 
 export default function Home() {
 	return (
 		<div>
 			<LandingHeader />
-=======
-import DropDownManager from '@/components/DropDownManager ';
-=======
-import DropDownManager from '@/components/DropDownManager';
->>>>>>> 22ec322 (FEAT : manager 드롭다운 / search  구현)
-=======
->>>>>>> c26df94 (index.tsx에서 컴포넌트 제거)
-import Header from '@/components/common/Header';
-export default function Home() {
-	return (
-		<div>
-			<Header />
-<<<<<<< HEAD
-			<DropDownManager />
->>>>>>> 4f5f081 (CHORE : react-select 설치)
-=======
->>>>>>> c26df94 (index.tsx에서 컴포넌트 제거)
 		</div>
 	);
 }
