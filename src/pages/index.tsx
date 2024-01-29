5f73899380505bd24e7dc7d0b8e02b2df7f23ffa
hyun522 <jhj1004v@naver.com>
2024-01-29 17:21:32 +0900

    FIX : _index  컴포넌트 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import LandingHeader from '@/components/common/Headers/LandingHeader';
-import DropDownProgress from '@/components/dropdown/DropDownProgress';
 
 export default function Home() {
 	return (
 		<div>
 			<LandingHeader />
-			<DropDownProgress />
 		</div>
 	);
 }
