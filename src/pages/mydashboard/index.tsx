620abd5848bec4d28b803e3ea26871f3567b62da
unknown <llqrw55@naver.com>
2024-01-30 14:24:34 +0900

    feat:대시보드 추가기능 구현

--- src/pages/mydashboard/index.tsx
+++ src/pages/mydashboard/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import MyDashboardHeader from '@/components/common/Headers/MyDashboardHeader';
-import AddDashboardButton from '@/components/common/Buttons/AddDashboardButton';
+import AddDashboardButton from '@/components/common/Buttons/addDashboardButton';
 import addIcon from '@/../../Public/assets/addIcon.svg';
 import DashboardButton from '@/components/domains/myDashBoard/DashboardButton';
 import PaginationButton from '@/components/domains/myDashBoard/PaginationButton';
@@ -13,21 +13,14 @@ import ColorChip from '@/components/common/chips/ColorChip';
 import { postDashboard } from '@/lib/api';
 import done from '@/../Public/assets/done.svg';
 import searchIcon from '@../../../Public/assets/searchIcon.svg';
+import PageLayout from '@/components/common/PageLayout';
 
 export default function MyDashBoard() {
 	const [addDashBoardModalOpen, setAddDashBoardModalOpen] = useState(false);
 	const [colorPick, setColorPick] = useState('');
-	const [NewDashBoardInfo, setNewDashBoardInfo] = useState<NewDashBoardInfo>({
-		id: 0,
-		title: '',
-		color: '',
-		createdAt: '',
-		updatedAt: '',
-		createdByMe: false,
-		userId: 0,
-	});
+	const [dashBoardList, setDashBoardList] = useState<DashBoardInfo[]>([]);
 
-	interface NewDashBoardInfo {
+	interface DashBoardInfo {
 		id: number;
 		title: string;
 		color: string;
@@ -56,110 +49,152 @@ export default function MyDashBoard() {
 	const colorList = ['green', 'purple', 'orange', 'blue', 'pink'];
 
 	const onSubmit = async () => {
-		if (getValues('dashboardName') && colorPick === '') {
+		if (getValues('dashboardName') && colorPick !== '') {
 			const data = await postDashboard({ title: getValues('dashboardName'), color: colorPick });
 			setAddDashBoardModalOpen(false);
+			setDashBoardList((prevDashBoardList) => [...prevDashBoardList, data]);
+		}
+	};
+
+	const handleChangHexadecimal = (color: string) => {
+		if (color === 'green') {
+			return '#7AC555';
+		} else if (color === 'purple') {
+			return '#760DDE';
+		} else if (color === 'orange') {
+			return '#FFA500';
+		} else if (color === 'blue') {
+			return '#76A6EA';
+		} else if (color === 'pink') {
+			return '#E876EA';
+		} else {
+			return '';
+		}
+	};
+
+	const handleChangColor = (color: string) => {
+		if (color === '#7AC555') {
+			return 'green';
+		} else if (color === '#760DDE') {
+			return 'purple';
+		} else if (color === '#FFA500') {
+			return 'orange';
+		} else if (color === '#76A6EA') {
+			return 'blue';
+		} else if (color === '#E876EA') {
+			return 'pink';
+		} else {
+			return '';
 		}
 	};
 
 	return (
 		<>
 			<MyDashboardHeader title={'내 대시보드'} nickname={'nickname'} profileImageUrl={''} />
-			<div className='flex '>
-				<div className='sm:gap:-[2.4rem] flex w-[100%] flex-col  gap-[4rem] bg-gray-F  px-[4rem] py-[4rem] sm:gap-[2.4rem]'>
-					<div className='flex w-[102.2rem] flex-col gap-[1.2rem] md:w-[50.4rem]  sm:w-[100%]'>
-						<div className='col-span-2 grid grid-cols-3 gap-[1.2rem] md:col-span-3 md:grid-cols-2 md:gap-[1rem] sm:col-span-1 sm:grid-cols-1 sm:gap-[0.8rem]'>
-							<AddDashboardButton
-								onClick={() => {
-									setAddDashBoardModalOpen(true);
-								}}
-							>
-								<Image className='px-[0.6rem] py-[0.6rem]' fill src={addIcon} alt='추가하기 아이콘' />
-							</AddDashboardButton>
-							<DashboardButton color='green' title='비브리지' />
-						</div>
-						<PaginationButton />
-					</div>
-
-					<div className='flex h-[100%] w-[102.2rem] flex-col gap-[2rem] rounded-[0.8rem]  bg-white px-[2.8rem] py-[3.2rem] md:w-[100%] sm:gap-[10.5rem] sm:px-[2.4rem] sm:py-[2.4rem]'>
-						<h2 className='text-24-700 sm:text-20-600'>초대받은 대시보드</h2>
-						<div className='relative'>
-							<Image
-								src={searchIcon}
-								className='absolute left-[1.4rem] top-[23%] h-[2.4rem] w-[2.4rem] sm:h-[2.2rem] sm:w-[2.2rem]'
-								alt='검색 아이콘'
-							/>
-							<input
-								placeholder='검색'
-								className='w-[100%] rounded-[0.6rem] border border-gray-D px-[4.2rem] py-[1.1rem] text-14-500 sm:px-[4.4rem]'
-							/>
-						</div>
-						<div className='mt-[0.4rem] flex items-center justify-start text-16-400 text-gray-9 '>
-							<div className='mr-[6rem] flex-grow'>이름</div>
-							<div className='flex-grow'>초대자</div>
-							<div className='flex-grow'>수락여부</div>
+			<PageLayout>
+				<div className='flex '>
+					<div className='sm:gap:-[2.4rem] flex w-[100%] flex-col  gap-[4rem] bg-gray-F  px-[4rem] py-[4rem] sm:gap-[2.4rem]'>
+						<div className='flex w-[102.2rem] flex-col gap-[1.2rem] md:w-[50.4rem]  sm:w-[100%]'>
+							<div className='col-span-2 grid grid-cols-3 gap-[1.2rem] md:col-span-3 md:grid-cols-2 md:gap-[1rem] sm:col-span-1 sm:grid-cols-1 sm:gap-[0.8rem]'>
+								<AddDashboardButton
+									onClick={() => {
+										setAddDashBoardModalOpen(true);
+									}}
+								>
+									<Image className='px-[0.6rem] py-[0.6rem]' fill src={addIcon} alt='추가하기 아이콘' />
+								</AddDashboardButton>
+								{dashBoardList?.map((dashboard) => (
+									<DashboardButton
+										key={dashboard.id}
+										color={handleChangColor(dashboard.color)}
+										title={dashboard.title}
+										createdByMe={dashboard.createdByMe}
+									/>
+								))}
+							</div>
+							<PaginationButton />
 						</div>
 
-						<div className='flex items-center justify-around border-b py-[2.7rem] text-16-500 text-black-3'>
-							<div className='flex-grow'>프로덕트 디자인</div>
-							<div className='flex-grow'>손동희</div>
-							<div className='flex flex-grow gap-[1rem]'>
-								<Button variant='confirm' color='violet'>
-									수락
-								</Button>
-								<Button variant='confirm' color='white'>
-									거절
-								</Button>
+						<div className='flex h-[100%] w-[102.2rem] flex-col gap-[2rem] rounded-[0.8rem]  bg-white px-[2.8rem] py-[3.2rem] md:w-[100%] sm:gap-[10.5rem] sm:px-[2.4rem] sm:py-[2.4rem]'>
+							<h2 className='text-24-700 sm:text-20-600'>초대받은 대시보드</h2>
+							<div className='relative'>
+								<Image
+									src={searchIcon}
+									className='absolute left-[1.4rem] top-[23%] h-[2.4rem] w-[2.4rem] sm:h-[2.2rem] sm:w-[2.2rem]'
+									alt='검색 아이콘'
+								/>
+								<input
+									placeholder='검색'
+									className='w-[100%] rounded-[0.6rem] border border-gray-D px-[4.2rem] py-[1.1rem] text-14-500 sm:px-[4.4rem]'
+								/>
+							</div>
+							<div className='mt-[0.4rem] flex items-center justify-start text-16-400 text-gray-9 '>
+								<div className='mr-[6rem] flex-grow'>이름</div>
+								<div className='flex-grow'>초대자</div>
+								<div className='flex-grow'>수락여부</div>
+							</div>
+
+							<div className='flex items-center justify-around border-b py-[2.7rem] text-16-500 text-black-3'>
+								<div className='flex-grow'>프로덕트 디자인</div>
+								<div className='flex-grow'>손동희</div>
+								<div className='flex flex-grow gap-[1rem]'>
+									<Button variant='confirm' color='violet'>
+										수락
+									</Button>
+									<Button variant='confirm' color='white'>
+										거절
+									</Button>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			{/* 대시보드 추가 모달 */}
-			<Layout
-				$modalType='Modal'
-				title='새로운 대시보드'
-				isOpen={addDashBoardModalOpen}
-				setOpen={setAddDashBoardModalOpen}
-			>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<div className=' flex flex-col justify-end gap-[1.2rem]'>
-						<FormInput<FormValue>
-							label='대시보드 이름'
-							name='dashboardName'
-							control={control}
-							rules={RULES.dashboardName}
-							required={!!('required' in RULES.dashboardName)}
-						/>
-						<div className='flex gap-[1rem]'>
-							{colorList.map((color) => (
-								<div key={color} className='relative'>
-									{color === colorPick ? (
-										<Image src={done} alt='체크 아이콘' className='absolute left-[0.3rem] top-[0.3rem] z-10' />
-									) : null}
-									<ColorChip key={color} color={color} onClick={() => setColorPick(color)} />
-								</div>
-							))}
+				{/* 대시보드 추가 모달 */}
+				<Layout
+					$modalType='Modal'
+					title='새로운 대시보드'
+					isOpen={addDashBoardModalOpen}
+					setOpen={setAddDashBoardModalOpen}
+				>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div className=' flex flex-col justify-end gap-[1.2rem]'>
+							<FormInput<FormValue>
+								label='대시보드 이름'
+								name='dashboardName'
+								control={control}
+								rules={RULES.dashboardName}
+								required={!!('required' in RULES.dashboardName)}
+							/>
+							<div className='flex gap-[1rem]'>
+								{colorList.map((color) => (
+									<div key={color} className='relative'>
+										{color === handleChangColor(colorPick) ? (
+											<Image src={done} alt='체크 아이콘' className='absolute left-[0.3rem] top-[0.3rem] z-10' />
+										) : null}
+										<ColorChip key={color} color={color} onClick={() => setColorPick(handleChangHexadecimal(color))} />
+									</div>
+								))}
+							</div>
+							<div className='flex justify-end gap-[1.2rem]'>
+								<Button
+									onClick={() => {
+										setAddDashBoardModalOpen(false);
+									}}
+									color='modalWhite'
+									disabled={false}
+									variant='modal'
+								>
+									취소
+								</Button>
+								<Button disabled={false} type='submit' color='modalViolet' variant='modal'>
+									확인
+								</Button>
+							</div>
 						</div>
-						<div className='flex justify-end gap-[1.2rem]'>
-							<Button
-								onClick={() => {
-									setAddDashBoardModalOpen(false);
-								}}
-								color='modalWhite'
-								disabled={false}
-								variant='modal'
-							>
-								취소
-							</Button>
-							<Button disabled={false} type='submit' color='modalViolet' variant='modal'>
-								확인
-							</Button>
-						</div>
-					</div>
-				</form>
-			</Layout>
+					</form>
+				</Layout>
+			</PageLayout>
 		</>
 	);
 }
