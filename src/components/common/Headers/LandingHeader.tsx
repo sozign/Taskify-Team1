150b51c174518cad7aace607dc2b1a02b0c48851
박지원 <jackgg12322@gmail.com>
2024-01-31 19:01:35 +0900

    fix : 랜딩 페이지 헤더 스타일 수정

--- src/components/common/Headers/LandingHeader.tsx
+++ src/components/common/Headers/LandingHeader.tsx
@@ -5,22 +5,19 @@ import Taskify from '@/../../Public/assets/Taskify.svg';
 
 export default function LandingHeader() {
 	return (
-		<header className='container m-3 bg-white md:pr-[2rem] sm:pr-[2rem] '>
-			<div className='flex items-center justify-between'>
-				<Link href='/' className='cursor-pointer'>
-					<div className='flex items-center p-5 md:mb-0'>
-						<Image alt='TaskifyImage' src={TaskifyImage} />
-						<Image alt='TaskifyLogo' src={Taskify} className='sm:hidden' />
-					</div>
+		<header className='container flex flex-row items-center justify-between px-[5rem] pb-[1.56rem] pt-[1.62rem]'>
+			<Link href='/' className='inline-flex  items-center justify-center pl-1 pr-[0.125rem] pt-1 '>
+				<Image alt='TaskifyImage' src={TaskifyImage} />
+				<Image alt='TaskifyLogo' src={Taskify} className='sm:hidden' />
+			</Link>
+
+			<div className='flex flex-row gap-[2.25rem]'>
+				<Link href='/login' className='text-10-400 font-normal text-black-3'>
+					로그인
 				</Link>
-				<nav className='flex items-center justify-center gap-[4rem] pr-[8rem] md:pr-[2rem] sm:pr-[2rem]'>
-					<Link href='/login' className=' text-16-500'>
-						로그인
-					</Link>
-					<Link href='/signup' className=' text-16-500'>
-						회원가입
-					</Link>
-				</nav>
+				<Link href='/signup' className='text-10-400 font-normal text-black-3'>
+					회원가입
+				</Link>
 			</div>
 		</header>
 	);
