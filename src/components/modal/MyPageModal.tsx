7e2d4ec184c2fb5df63616467fc738b57f3e39ea
hyun522 <jhj1004v@naver.com>
2024-01-31 11:40:37 +0900

    FEAT : 모달창 구현 후 화면에서 open 될수 있도록 세팅

---
+++ src/components/modal/MyPageModal.tsx
@@ -0,0 +1,23 @@
+import { Dispatch, SetStateAction } from 'react';
+import Layout from './Layout';
+import Button from '../common/Buttons/Button';
+
+interface ModalProps {
+	isOpen: boolean;
+	setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+function MyPageModal({ isOpen, setOpen }: ModalProps) {
+	return (
+		<Layout $modalType='Alert' isOpen={isOpen} setOpen={setOpen}>
+			<div>
+				<p className='text-[1.8rem]'>현재 비밀번호가 틀렸습니다.</p>
+				<Button color='modalViolet' variant='modal' disabled={false} className='relative left-[22.4rem] top-[4.4rem]'>
+					확인
+				</Button>
+			</div>
+		</Layout>
+	);
+}
+
+export default MyPageModal;
