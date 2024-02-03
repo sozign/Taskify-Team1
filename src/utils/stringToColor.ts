e32d66491cc934588de50e605c480a65870e27df
So-Yeon Ahn <sozygn@gmail.com>
2024-02-03 17:04:58 +0900

    refactor: stringToNumber 함수 커스텀

--- src/utils/stringToColor.ts
+++ src/utils/stringToColor.ts
@@ -1,4 +1,4 @@
-const stringToNumber = (str: string): number => {
+const stringToNumber = (str: string, number: number): number => {
 	let hash = 0;
 
 	// 문자의 ASCII 코드를 합산
@@ -6,9 +6,9 @@ const stringToNumber = (str: string): number => {
 		hash += str.charCodeAt(i);
 	}
 
-	// 정규화 및 0~4까지의 값으로 변환
+	// 정규화 및 0~(number-1)까지의 값으로 변환
 	const normalizedValue = (hash % 1000) / 1000; // 0부터 1 사이의 값
-	const mappedNumber = Math.floor(normalizedValue * 4); // 0부터 3 사이의 값
+	const mappedNumber = Math.floor(normalizedValue * number);
 
 	return mappedNumber;
 };
