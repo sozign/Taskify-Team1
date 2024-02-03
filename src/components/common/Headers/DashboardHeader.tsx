8a179c0c17fa31feeba34d0c917643eecca72ac6
박지원 <jackgg12322@gmail.com>
2024-02-03 14:46:53 +0900

    fix : 대시보드헤더 일부 기능구현

--- src/components/common/Headers/DashboardHeader.tsx
+++ src/components/common/Headers/DashboardHeader.tsx
@@ -4,22 +4,29 @@ import { useEffect, useRef, useState } from 'react';
 import InviteModal from '@/components/modal/InviteModal';
 import { useRouter } from 'next/router';
 import HeaderNavDropdown from './HeaderNavDropdown';
+import { getUsers } from '@/lib/api';
 
-interface userDataProps {
+interface HeaderNavProps {
 	dashboardId: number;
-	title: string;
-	nickname: string;
-	profileImageUrl: string;
+	title?: string | null;
 }
 
-export default function DashboardHeader({ dashboardId, title, nickname, profileImageUrl }: userDataProps) {
+export default function DashboardHeader({ dashboardId, title }: HeaderNavProps) {
 	// 모달 1 열림, 닫힘 제어
 	const [isTaskEditModalOpen, setIsTaskEditModalOpen] = useState(false);
 	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+	const [userData, setUserData] = useState<string>('');
+
 	const router = useRouter();
 	const dropDownRef = useRef<HTMLDivElement>(null);
 
+	const userProfileData = async () => {
+		const { ...response } = await getUsers();
+		setUserData(response?.nickname);
+	};
+
 	useEffect(() => {
+		userProfileData();
 		const clickOutside = (e: MouseEvent) => {
 			if (isDropdownOpen && !dropDownRef.current?.contains(e.target as Node)) {
 				setIsDropdownOpen(false);
@@ -40,21 +47,36 @@ export default function DashboardHeader({ dashboardId, title, nickname, profileI
 	return (
 		<>
 			<header className='container fixed inset-0  z-10  flex h-[7rem] flex-row items-center justify-between  bg-white   pl-[4rem] sm:h-[6rem]'>
-				<Link href='/mydashboard' className=' flex flex-row gap-[0.4rem]  text-20-700 text-black-3'>
-					<span className='md:hidden sm:hidden'>{title}</span>
-					{router.pathname.startsWith('/mydashboard') ? null : (
-						<Image
-							alt='대시보드 왕관 아이콘'
-							src='/assets/royalCrownIcon.svg'
-							width={20}
-							height={20}
-							className=' w-[2.103rem]  md:hidden sm:hidden'
-						/>
-					)}
-				</Link>
+				{router.pathname.startsWith('/mypage') ? (
+					<div className=' flex flex-row gap-[0.4rem]  text-20-700 text-black-3'>
+						<span className='md:hidden sm:hidden'>{title}</span>
+						{router.pathname.startsWith('/mydashboard') || router.pathname.startsWith('/mypage') ? null : (
+							<Image
+								alt='대시보드 왕관 아이콘'
+								src='/assets/royalCrownIcon.svg'
+								width={20}
+								height={20}
+								className=' w-[2.103rem]  md:hidden sm:hidden'
+							/>
+						)}
+					</div>
+				) : (
+					<Link href='/mydashboard' className=' flex flex-row gap-[0.4rem]  text-20-700 text-black-3'>
+						<span className='md:hidden sm:hidden'>{title}</span>
+						{router.pathname.startsWith('/mydashboard') || router.pathname.startsWith('/mypage') ? null : (
+							<Image
+								alt='대시보드 왕관 아이콘'
+								src='/assets/royalCrownIcon.svg'
+								width={20}
+								height={20}
+								className=' w-[2.103rem]  md:hidden sm:hidden'
+							/>
+						)}
+					</Link>
+				)}
 
 				<nav className='flex flex-row items-center  gap-[1.6rem]'>
-					{router.pathname.startsWith('/mydashboard') ? null : (
+					{router.pathname.startsWith('/mydashboard') || router.pathname.startsWith('/mypage') ? null : (
 						<>
 							<button
 								className='flex h-[4rem] w-[8.8rem] flex-shrink-0 flex-row items-center justify-center gap-[0.8rem] rounded-[0.8rem] border-2 border-gray-D bg-white md:h-[3.6rem] md:w-[8.5rem] sm:h-[3rem] sm:w-[4.9rem]'
@@ -86,12 +108,10 @@ export default function DashboardHeader({ dashboardId, title, nickname, profileI
 								<span className='items-center text-16-500 text-gray-7'>초대하기</span>
 							</button>
 
-							<div>
-								{/* <Image alt='프로필 이미지'
-              width={20}
-							height={20}
-                 src={profileImageUrl || ColorBlue} className='relative' /> */}
-								<span className=' items-center text-center text-16-600 text-white '>{nickname.slice(0, 1)}</span>
+							<div className='flex h-[3.8rem] w-[3.8rem] items-center justify-center rounded-full border-2 border-white bg-black-0'>
+								<span className='flex items-center justify-center text-center text-16-600 text-white '>
+									{userData.toUpperCase().slice(0, 1)}
+								</span>
 							</div>
 							<Image
 								alt='영역 나누는 라인 이미지'
@@ -108,14 +128,12 @@ export default function DashboardHeader({ dashboardId, title, nickname, profileI
 							ref={dropDownRef}
 							onClick={() => setIsDropdownOpen(!isDropdownOpen)}
 						>
-							<div className='relative'>
-								{/* <Image alt='프로필 이미지' 
-              width={20}
-							height={20}
-              src={profileImageUrl || ColorBlue} className='relative' /> */}
-								<span className=' items-center text-center text-16-600 text-white '>{nickname.slice(0, 1)}</span>
+							<div className='flex h-[3.8rem] w-[3.8rem] items-center justify-center rounded-full border-2 border-white bg-black-0'>
+								<span className='flex items-center justify-center text-center text-16-600 text-white '>
+									{userData.toUpperCase().slice(0, 1)}
+								</span>
 							</div>
-							<span className='text-center text-16-600 sm:hidden'>{nickname}</span>
+							<span className='text-center text-16-600 sm:hidden'>{userData}</span>
 							<div className='absolute translate-y-[5rem] pr-[2rem] sm:pr-[7rem]'>
 								{isDropdownOpen && <HeaderNavDropdown />}
 							</div>
