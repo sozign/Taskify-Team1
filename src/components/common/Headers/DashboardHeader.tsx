d1618486cafb080a5d478eba88ecbe19e72284bd
박지원 <jackgg12322@gmail.com>
2024-02-03 17:07:46 +0900

    feat : 대시보드 이미지 경로 수정 및 타이틀 수정

--- src/components/common/Headers/DashboardHeader.tsx
+++ src/components/common/Headers/DashboardHeader.tsx
@@ -4,7 +4,12 @@ import { useEffect, useRef, useState } from 'react';
 import InviteModal from '@/components/modal/InviteModal';
 import { useRouter } from 'next/router';
 import HeaderNavDropdown from './HeaderNavDropdown';
-import { getUsers } from '@/lib/api';
+import { getDashboardItem, getUsers } from '@/lib/api';
+import { DashboardData } from '@/constants/types';
+import royalCrownIcon from '@/../Public/assets/royalCrownIcon.svg';
+import settingIcon from '@/../Public/assets/settingIcon.svg';
+import PlusIcon from '@/../Public/assets/PlusIcon.svg';
+import Vector from '@/../Public/assets/Vector.svg';
 
 interface HeaderNavProps {
 	dashboardId: number;
@@ -18,15 +23,36 @@ export default function DashboardHeader({ dashboardId, title }: HeaderNavProps) 
 	const [userData, setUserData] = useState<string>('');
 
 	const router = useRouter();
+	const boardId = +(router.query.boardid ?? 0);
 	const dropDownRef = useRef<HTMLDivElement>(null);
 
+	const [dashboardInfo, setDashboardInfo] = useState<DashboardData>({
+		id: boardId,
+		title: '',
+		color: '#7AC555',
+		createdAt: '',
+		updatedAt: '',
+		createdByMe: true,
+		userId: 0,
+	});
 	const userProfileData = async () => {
 		const { ...response } = await getUsers();
 		setUserData(response?.nickname);
 	};
 
+	async function loadDashboardData(dashboardId: number) {
+		try {
+			const resData = await getDashboardItem(dashboardId);
+			setDashboardInfo(resData);
+			console.log('대시보드 로드', resData);
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	}
+
 	useEffect(() => {
 		userProfileData();
+		loadDashboardData(boardId);
 		const clickOutside = (e: MouseEvent) => {
 			if (isDropdownOpen && !dropDownRef.current?.contains(e.target as Node)) {
 				setIsDropdownOpen(false);
@@ -49,28 +75,20 @@ export default function DashboardHeader({ dashboardId, title }: HeaderNavProps) 
 			<header className='container fixed inset-0  z-10  flex h-[7rem] flex-row items-center justify-between  bg-white   pl-[4rem] sm:h-[6rem]'>
 				{router.pathname.startsWith('/mypage') ? (
 					<div className=' flex flex-row gap-[0.4rem]  text-20-700 text-black-3'>
-						<span className='md:hidden sm:hidden'>{title}</span>
+						<span className='md:hidden sm:hidden'>{dashboardInfo.title || title}</span>
 						{router.pathname.startsWith('/mydashboard') || router.pathname.startsWith('/mypage') ? null : (
 							<Image
 								alt='대시보드 왕관 아이콘'
 								src='/assets/royalCrownIcon.svg'
-								width={20}
-								height={20}
 								className=' w-[2.103rem]  md:hidden sm:hidden'
 							/>
 						)}
 					</div>
 				) : (
 					<Link href='/mydashboard' className=' flex flex-row gap-[0.4rem]  text-20-700 text-black-3'>
-						<span className='md:hidden sm:hidden'>{title}</span>
+						<span className='md:hidden sm:hidden'>{dashboardInfo.title || title}</span>
 						{router.pathname.startsWith('/mydashboard') || router.pathname.startsWith('/mypage') ? null : (
-							<Image
-								alt='대시보드 왕관 아이콘'
-								src='/assets/royalCrownIcon.svg'
-								width={20}
-								height={20}
-								className=' w-[2.103rem]  md:hidden sm:hidden'
-							/>
+							<Image alt='대시보드 왕관 아이콘' src={royalCrownIcon} className=' w-[2.103rem]  md:hidden sm:hidden' />
 						)}
 					</Link>
 				)}
@@ -84,9 +102,7 @@ export default function DashboardHeader({ dashboardId, title }: HeaderNavProps) 
 							>
 								<Image
 									alt='관리 버튼 셋팅 아이콘'
-									src='/assets/settingIcon.svg'
-									width={20}
-									height={20}
+									src={settingIcon}
 									className='flex h-[2rem] w-[2rem] flex-shrink-0 flex-row items-center sm:hidden'
 								/>
 								<span className='items-center text-16-500 text-gray-7'>관리</span>
@@ -100,9 +116,7 @@ export default function DashboardHeader({ dashboardId, title }: HeaderNavProps) 
 							>
 								<Image
 									alt='초대하기 버튼 플러스 아이콘'
-									src='/assets/PlusIcon.svg'
-									width={20}
-									height={20}
+									src={PlusIcon}
 									className='flex h-[2rem] w-[2rem] flex-shrink-0 flex-row items-center sm:hidden'
 								/>
 								<span className='items-center text-16-500 text-gray-7'>초대하기</span>
@@ -113,13 +127,7 @@ export default function DashboardHeader({ dashboardId, title }: HeaderNavProps) 
 									{userData.toUpperCase().slice(0, 1)}
 								</span>
 							</div>
-							<Image
-								alt='영역 나누는 라인 이미지'
-								width={20}
-								height={20}
-								src='/assets/Vector.svg'
-								className='h-[3.8rem] '
-							/>
+							<Image alt='영역 나누는 라인 이미지' src={Vector} className='h-[3.8rem] ' />
 						</>
 					)}
 					<div className='flex flex-row items-center justify-center gap-[0.9rem]'>
