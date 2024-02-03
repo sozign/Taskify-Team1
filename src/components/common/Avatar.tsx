586ec39afd60ceb81fceaca4691fa337790e1f47
So-Yeon Ahn <sozygn@gmail.com>
2024-02-03 17:06:09 +0900

    feat: 아바타 공용 컴포넌트 제작

---
+++ src/components/common/Avatar.tsx
@@ -0,0 +1,26 @@
+import stringToNumber from '@/utils/stringToColor';
+import clsx from 'clsx';
+
+interface AvatarProps {
+	name: string;
+	className: string;
+	withBorder?: boolean;
+}
+
+export default function Avatar({ name, className, withBorder = false }: AvatarProps) {
+	const pickerNumber = stringToNumber(name, 5);
+	const MAPPER = ['bg-[#FFC85A]', 'bg-[#FDD446]', 'bg-[#9DD7ED]', 'bg-[#C4B1A2]', 'bg-[#A3C4A2]'];
+
+	return (
+		<div
+			style={{ boxShadow: `${withBorder && '0 0 0 0.2rem white'}` }}
+			className={clsx(
+				className,
+				MAPPER[pickerNumber],
+				'text-montserrat flex items-center justify-center rounded-[50%] text-center text-12-600 text-white',
+			)}
+		>
+			{name.slice(0, 1)}
+		</div>
+	);
+}
