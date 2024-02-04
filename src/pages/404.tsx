80e845fbb3f685947062331461a148d3e6612a38
박운성 <cheanju0@gmail.com>
2024-02-05 08:49:23 +0900

    design: 404 페이지 반응형 디자인 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	const router = useRouter();
 	return (
 		<div className='flex h-[100vh] items-center justify-center'>
-			<div className='flex h-[30rem] gap-[4rem]'>
+			<div className='flex h-[30rem] gap-[4rem] md:flex-col md:items-center md:justify-center md:px-[4rem] sm:px-[2rem]'>
 				<Image src={notFound} alt='404 페이지 이미지' width={500} height={300} />
 				<div className='flex flex-col items-center justify-center gap-10 text-22-500'>
 					<div className='text-40-700'>웹 페이지를 찾을 수 없습니다.</div>
