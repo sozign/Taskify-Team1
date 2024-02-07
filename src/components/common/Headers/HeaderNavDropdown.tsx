8e01f0f0bfecd9497084b25d7b910142c4117024
So-Yeon Ahn <sozygn@gmail.com>
2024-02-07 17:43:35 +0900

    design: 헤더 드롭다운 디자인/위치 수정

--- src/components/common/Headers/HeaderNavDropdown.tsx
+++ src/components/common/Headers/HeaderNavDropdown.tsx
@@ -9,15 +9,18 @@ export default function HeaderNavDropdown() {
 		router.push('/');
 	};
 	return (
-		<div className='h-[8.2rem] w-[9.3rem] flex-shrink-0 rounded-[0.6rem] border-solid border-gray-D bg-white shadow-[0_0.4rem_2rem_0] shadow-black-0 sm:w-[7.3rem]'>
+		<div className='w-[9.3rem] flex-shrink-0 rounded-[0.6rem] border-solid border-gray-D bg-white px-[0.6rem] py-[0.6rem] shadow-[30px_30px_100px_-18px_rgba(0,0,0,0.67)] shadow-black-0'>
 			<ul className='text-center text-14-400 leading-[2.4rem]'>
-				<li onClick={handleLogout} className='hover:rounded-[0.4rem] hover:bg-violet-F hover:text-violet-5'>
+				<li
+					onClick={handleLogout}
+					className='flex h-[3.2rem] items-center justify-center hover:rounded-[0.4rem] hover:bg-violet-F hover:text-violet-5'
+				>
 					로그아웃
 				</li>
-				<li className='hover:rounded-[0.4rem] hover:bg-violet-F hover:text-violet-5'>
+				<li className='flex h-[3.2rem] items-center justify-center hover:rounded-[0.4rem] hover:bg-violet-F hover:text-violet-5'>
 					<Link href='/mypage'>내정보</Link>
 				</li>
-				<li className='hover:rounded-[0.4rem] hover:bg-violet-F hover:text-violet-5'>
+				<li className='flex h-[3.2rem] items-center justify-center hover:rounded-[0.4rem] hover:bg-violet-F hover:text-violet-5'>
 					<Link href='/mydashboard'> 내 대시보드</Link>
 				</li>
 			</ul>
