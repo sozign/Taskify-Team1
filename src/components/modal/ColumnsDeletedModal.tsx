a837432f3ad76320a2f08670e483c3d14b137e21
Park Ji Won <124119421+PJW980921@users.noreply.github.com>
2024-02-07 15:48:21 +0900

    feat : 컬럼 수정 모달창 구현 (#143)
    * fix : 아바타 이미지 깨짐 현상 수정
    
    * feat : 컬럼 수정하기 모달 구현
    
    * feat : 삭제하기 모달 구현
    
    * fix : 에러 발생 수정
    
    * refactor : 불필요 선언 제거

---
+++ src/components/modal/ColumnsDeletedModal.tsx
@@ -0,0 +1,56 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import Button from '../common/Buttons/Button';
+import Layout from './Layout';
+import { deleteColumn } from '@/lib/api';
+import { useRouter } from 'next/router';
+
+interface ModalProps {
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+	columnId: number;
+}
+
+export default function ColumnsDeletedModal({ columnId, isOpen, setIsOpen }: ModalProps) {
+	const [isColumnId, setIsColumnId] = useState<number>(columnId);
+	const router = useRouter();
+	const columnDeleteHandler = async () => {
+		await deleteColumn(isColumnId);
+		setIsColumnId(isColumnId);
+		router.reload();
+		setIsOpen((prev) => !prev);
+	};
+	return (
+		<>
+			<Layout $modalType='Modal' isOpen={isOpen} setOpen={setIsOpen}>
+				<div className='flex flex-col gap-[4rem]'>
+					<div className='flex flex-row items-center justify-center'>
+						<span className=' text-14-400'>컬럼의 모든 카드가 삭제 됩니다.</span>
+					</div>
+					<div className='flex flex-row justify-end gap-[1.2rem]'>
+						<Button
+							onClick={() => {
+								setIsOpen(false);
+							}}
+							color='modalWhite'
+							disabled={false}
+							variant='modal'
+						>
+							취소
+						</Button>
+						<Button
+							onClick={() => {
+								columnDeleteHandler();
+							}}
+							disabled={false}
+							type='submit'
+							color='modalViolet'
+							variant='modal'
+						>
+							삭제
+						</Button>
+					</div>
+				</div>
+			</Layout>
+		</>
+	);
+}
